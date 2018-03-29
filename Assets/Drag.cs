cac0d002c22558c11b665c2c41c5d7f339ac4210
Pasdrus <baptiste.desprets@gmail.com>
2018-03-29 23:03:20 +0200

    CommitBase

---
+++ Assets/Drag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDragged");
+    }
+
+}
