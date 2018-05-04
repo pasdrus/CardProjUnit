1a0826afa4b4ab7c49ce33bd2707010804b2d7ed
Kizerx <yoh59165@gmail.com>
2018-05-04 19:30:00 +0200

    Cartes

---
+++ Assets/deck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+//[CreateAssetMenu(fileName = "New deck", menuName = "deck")]
+public class deck : MonoBehaviour
+{
+
+    // public List<Card> deckliste = new List<Card>();
+    public List<Card1> deckliste;
+    //public Card card;
+
+  /*  void start()
+    {
+       Card card = ScriptableObject.CreateInstance<Card>();
+        for (int i = 0; i < deckliste.Count; i++)
+        {
+            deckliste[i] = card;
+        }
+        }*/
+    }
+
+  
