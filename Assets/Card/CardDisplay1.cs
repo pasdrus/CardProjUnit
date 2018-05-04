fffe0eafd5c87d5c24b4244c49fcbfe65dff73a4
Kizerx <yoh59165@gmail.com>
2018-05-04 16:24:03 +0200

    carte scriptable et menu

---
+++ Assets/Card/CardDisplay1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class CardDisplay1 : MonoBehaviour
+{
+
+    public Card1 card;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image artworkImage;
+
+    public Text staminaText;
+    public Text multAttackText;
+    public Text multElemText;
+    public Text nbTurnText;
+    public Text nbDesText;
+
+    // Use this for initialization
+    void Start()
+    {
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+
+        artworkImage.sprite = card.artwork;
+
+        staminaText.text = card.StaminaCost.ToString();
+        multAttackText.text = card.multAttack.ToString();
+        multElemText.text = card.multElem.ToString();
+        nbTurnText.text = card.nbTurn.ToString();
+        nbDesText.text = card.nbDes.ToString();
+    }
+
+}
---
+++ Assets/ScriptMenu/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+        public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Deck()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void BackDeck()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
