95537b96373c72549b8af66b6c17f865d1c32469
Kizerx <yoh59165@gmail.com>
2018-05-09 14:52:41 +0200

    s

--- Assets/SaveJson/SetCardInfo.cs
+++ Assets/SaveJson/SetCardInfo.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using System;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class SetCardInfo : MonoBehaviour
 {
@@ -10,6 +12,10 @@ public class SetCardInfo : MonoBehaviour
 
     //private deck _currentCard;
     public deck _currentCard;
+    public listedecks listed;
+    String namefichier;
+
+
 
     void Start()
     {
@@ -24,10 +30,13 @@ public class SetCardInfo : MonoBehaviour
             SaveData();
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+      if (Input.GetKeyDown(KeyCode.R))
         {
-            ReadData();
+            ReadDataDeckliste();
         }
+
+
+        // ReadData(namefichier);
     }
 
 
@@ -39,7 +48,8 @@ public class SetCardInfo : MonoBehaviour
         {
             int i = 0;
             //Sauvegarde dans C:\Users\Admin Flo\AppData\LocalLow\DefaultCompany\CardGame3D ! Pour chez moi.
-            string path = Application.persistentDataPath + "/deck.json";
+            // string path = Application.persistentDataPath + "/deck.json";
+            string path = Application.persistentDataPath + "/" + namefichier + ".json";
             _currentCard = GetComponent<deck>();
 
             // On déclare un tableau de Card de la taille de la deckListe.
@@ -56,14 +66,36 @@ public class SetCardInfo : MonoBehaviour
             Debug.Log(cardsToJson);
             // on ecrit dans le fichier
             System.IO.File.WriteAllText(path, cardsToJson);
+
+            // quand on sauvegarde le deck , on l'ajoute au fichier xxdeckxx.json 
+            string[] fis;
+           //  if (listed.deckls.Count == 0) { fis = new string[1]; fis[0] = namefichier; }
+          //    else
+          //   {
+                fis = new string[listed.deckls.Count+1];
+
+                int j = 0;
+                foreach (String s in listed.deckls)
+                {
+                    fis[j] = s;
+                    j++;
+
+                }
+                fis[listed.deckls.Count] = namefichier;
+           // }
+           
+            string fi = JsonHelper.ToJson(fis, true);
+            path = Application.persistentDataPath + "/xxdeckxx.json";
+            System.IO.File.WriteAllText(path, fi);
         }
     }
 
-    public void ReadData()
+    public void ReadData(string test)
     {
-        string path = Application.persistentDataPath + "/deck.json";
+       string path = Application.persistentDataPath + "/" + test + ".json";
+       // string path = Application.persistentDataPath + "/deck.json";
         //On lit tout le fichier json
-        string contents = System.IO.File.ReadAllText(path);
+       string contents = System.IO.File.ReadAllText(path);
 
         //On recupère les objets dans le fichier .json à l'aide de la classe JsonHelper et on les stocks dans un Array de Card
         Card1[] card = JsonHelper.FromJson<Card1>(contents);
@@ -75,8 +107,26 @@ public class SetCardInfo : MonoBehaviour
            _currentCard.deckliste.Add(c);
           
         }
+        }
+
+    public void ReadDataDeckliste()
+    {
+        string path = Application.persistentDataPath + "/xxdeckxx.json";
+        string contents = System.IO.File.ReadAllText(path);
+        String[] fis = JsonHelper.FromJson<String>(contents);
+        Debug.Log(contents);
+        for (int i = 0; i < fis.Length; i++)
+        {
+
+            String s = fis[i]; Debug.Log(s);
+            listed.deckls.Add(s);
+        }
+    }
 
 
+    public void Textchange(string newText)
+    {
+        namefichier = newText;
     }
 
 }
--- Assets/addObjectTolist.cs
+++ Assets/addObjectTolist.cs
@@ -54,5 +54,4 @@ public class addObjectTolist : MonoBehaviour
  }
 
 
-
 }
