95b43469443bee9c1af4a4761d0bc04ec6873416
Kizerx <yoh59165@gmail.com>
2018-05-10 13:31:14 +0200

    compteur carte

---
+++ Assets/decks/nbcarte.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nbcarte : MonoBehaviour {
+
+    public deck _currentCard;
+    public Text myText;
+
+
+    public void Update()
+    {
+        changeNumber();
+    }
+
+    public void changeNumber()
+    {
+        myText.text = _currentCard.deckliste.Count  + "/15";
+    }
+
+
+}
